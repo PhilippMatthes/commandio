28941a2e6a7e3cdeb322040c61f547756a1546ab
PhilippMatthes <ffxdesigns@gmail.com>
2018-08-28 16:51:12 +0200

    Commit first sources

---
+++ command.io/Assets/Behaviours/LinkNavigationVelocityToAnimationSpeed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof (NavMeshAgent))]
+[RequireComponent (typeof (Animator))]
+public class LinkNavigationVelocityToAnimationSpeed : MonoBehaviour {
+
+    private Animator animator;
+    private NavMeshAgent agent;
+    public float multiplier = 0.01f;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+	}
+
+    void Update()
+    {
+        animator.speed = (agent.velocity.magnitude / Time.deltaTime) * multiplier;
+    }
+
+}
