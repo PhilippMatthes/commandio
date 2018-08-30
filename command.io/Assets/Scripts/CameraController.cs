9eaf80fe60e96141d6ce89663c931870cd08073d
PhilippMatthes <ffxdesigns@gmail.com>
2018-08-30 17:06:54 +0200

    Add oil tower

--- command.io/Assets/Scripts/CameraController.cs
+++ command.io/Assets/Scripts/CameraController.cs
@@ -14,8 +14,51 @@ public class CameraController : MonoBehaviour {
 
     float smoothing = 0.0f;
 
-	void LateUpdate () {
+    private bool onHandheld;
 
+    void Start()
+    {
+        onHandheld = SystemInfo.deviceType == DeviceType.Handheld;
+    }
+
+    void LateUpdate () {
+        if (onHandheld)
+        {
+            UpdateHandheldMovement();
+        }
+        else
+        {
+            UpdateMovement();
+        }
+        
+	}
+
+    private void UpdateHandheldMovement()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            transform.Translate(-touchDeltaPosition.x * panSpeed, -touchDeltaPosition.y * panSpeed, 0);
+        } else if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            Camera.main.orthographicSize += deltaMagnitudeDiff * 0.1f;
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 5, 20);
+        }
+    }
+
+    private void UpdateMovement()
+    {
         Vector3 pos = transform.position;
         Vector2 mouse = Input.mousePosition;
         float screenHeight = Screen.height;
@@ -25,7 +68,6 @@ public class CameraController : MonoBehaviour {
         float deltaLowerScreenEdge = Mathf.Clamp(mouse.y, 0, screenHeight);
         float deltaRightScreenEdge = Mathf.Clamp(screenWidth - mouse.x, 0, screenWidth);
         float deltaLeftScreenEdge = Mathf.Clamp(mouse.x, 0, screenWidth);
-
 
         if (deltaUpperScreenEdge < panBorderThickness)
         {
@@ -62,11 +104,15 @@ public class CameraController : MonoBehaviour {
             smoothing += smoothingSpeed;
             Vector3 smoothed = Vector3.Lerp(transform.position, pos, smoothing);
             transform.position = smoothed;
-        } else
+
+        }
+        else
         {
             smoothing = 0f;
         }
 
         smoothing = Mathf.Clamp(smoothing, 0.0f, 1.0f);
-	}
+    }
+
+
 }
---
+++ command.io/Assets/Scripts/MainGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Application.targetFrameRate = 60;
+        QualitySettings.antiAliasing = 0;
+        QualitySettings.vSyncCount = 0;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+	
+}
